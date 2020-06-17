868d9cd3c16ce4663c7855f66d94c6bc2bc12339
Hannah Bregman <bregmanhannah@gmail.com>
2020-06-17 20:11:44 +0000

    added ajax requests to display tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,98 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+const createTweetElement = function (tweetData) {
+  let $tweet = $(`<article class="tweet">
+  <header>
+    <div class="user">
+      <img src="${tweetData.user.avatars}">
+      <h5>Newton</h5>
+    </div>
+    <div class="user-name">${tweetData.user.handle}</div>
+  </header>
+  <div class="content">${tweetData.content.text}</div>
+  <footer>${Date(tweetData.created_at)}
+    <div class="footer-images"><img src="/images/flag.png">
+      <img src="/images/heart.png">
+      <img src="/images/retweet.png">
+    </div>
+  </footer>
+</article>`);
+
+  return $tweet;
+};
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (let tweet of tweets) {
+    let $result = createTweetElement(tweet);
+    $('div.tweets-container').prepend($result);
+  }
+}
+
+const submitForm = function () {
+  $('form').submit(function (evt) {
+    evt.preventDefault();
+    console.log('tweeted!')
+    let tweet = $("textarea[name='text']").val();
+    if (tweet === "") {
+      alert("Your tweet seems empty");
+    } else if (tweet.length > 140) {
+      alert("Your tweet is too long");
+    } else {
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        dataType: 'JSON',
+        data: $(this).serialize(),
+      }).then(function (response) {
+        loadTweets();
+        
+      })
+    }
+  })
+}
+
+const loadTweets = function () {
+  $.ajax({
+    url: 'http://localhost:8080/tweets',
+    method: 'GET',
+    dataType: 'JSON',
+  }).then(function (response) {
+    renderTweets(response);
+  })
+};
+
+$(document).ready(() => {
+  loadTweets();
+  submitForm();
+  
+
+});
--- public/scripts/hover-popup.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  //getting the length of the input in the textarea and updating the counter
-  $('article.tweet').hover(function() {
-    
-    $(this).find('div.signature').html('<img src="/images/newton-signature.png">');
-    
-  });
-  
-});
