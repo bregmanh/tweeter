4951c277d5011dae5896581db9c86d1a920efbe3
Hannah Bregman <bregmanhannah@gmail.com>
2020-06-18 20:31:11 +0000

    linted and cleaned the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(() => {
   loadTweets();
   submitForm();
-
-
 });
+
 const data = [
   {
     "user": {
@@ -34,7 +28,8 @@ const data = [
     "created_at": 1461113959088
   }
 ];
-const createTweetElement = function (tweetData) {
+
+const createTweetElement = function(tweetData) {
   let $tweet = $(`<article class="tweet">
   <header>
     <div class="user">
@@ -55,38 +50,35 @@ const createTweetElement = function (tweetData) {
   return $tweet;
 };
 
-const submitForm = function () {
+const submitForm = function() {
   $('form').on("submit", function (evt) {
     evt.preventDefault();
     let tweet = $("textarea[name='text']").val();
     $('.error-msg').empty();
     $('.error-msg').css('display', 'none');
 
-
     if (tweet === "") {
-      $('.error-msg').css('display','flex');
+      $('.error-msg').css('display', 'flex');
       $('.error-msg').append(`<p><i class="fa fa-warning" aria-hidden="true"></i>Your tweet seems empty</p>`);
     } else if (tweet.length > 140) {
-      $('.error-msg').css('display','flex');
+      $('.error-msg').css('display', 'flex');
       $('.error-msg').append(`<p><i class="fa fa-warning" aria-hidden="true"></i>Your tweet is too long!</p>`);
     } else {
-      $('.error-msg').css('display','flex');
+      $('.error-msg').css('display', 'flex');
       $('.error-msg').empty();
       $('.error-msg').hide();
       $.ajax('/tweets', {
         method: 'POST',
         data: $(this).serialize(),
       }).then(() => {
-        
         loadTweets();
         $("#tweet-text").val("");
-
-      })
+      });
     }
-  })
-}
+  });
+};
 
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
@@ -96,41 +88,39 @@ const renderTweets = function (tweets) {
   }
 }
 
-const loadTweets = function () {
+const loadTweets = function() {
   $.ajax({
     url: 'http://localhost:8080/tweets',
     method: 'GET',
     dataType: 'JSON',
   }).then(function (response) {
     renderTweets(response);
-  })
+  });
 };
 
-const escape = function (str) {
+const escape = function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
-const timePosted = function (date) {
+const timePosted = function(date) {
   let now = new Date(Date.now());
   let posted = new Date(date);
   let seconds = now.getSeconds() - posted.getSeconds();
   if (seconds < 60) {
-    return "less than 1min ago"
+    return "less than 1min ago";
   } else if (seconds >= 60 && seconds < 3600) {
     let mins = Math.floor(seconds / 60);
-    return `about ${mins} ago`
+    return `about ${mins} ago`;
   } else if (seconds >= 3600 && seconds < 86400) {
     let hrs = Math.floor(seconds / 3600);
-    return `about ${hrs} ago`
-
+    return `about ${hrs} ago`;
   } else {
     let days = Math.floor(seconds / 86400);
-    return `about ${days} ago`
-
+    return `about ${days} ago`;
   }
-}
+};
 
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
   //getting the length of the input in the textarea and updating the counter
   $('.new-tweet').on('keyup', function (event) {
     let tweetLength = $(this).find('textarea').val().length;
     $(this).find('.counter').html(function () {
       if (140 - tweetLength < 0) {
         $(this).css("color", "red");
-        return (140 - tweetLength)
+        return (140 - tweetLength);
       }
       $(this).css("color", "black");
       return (140 - tweetLength);
     });
   });
   //resets the character count to 140 after form submission
-  $('form').on('submit', function (event) {
-      return $('.counter').html(140); 
-    });
-  
+  $('form').on('submit', function(event) {
+    return $('.counter').html(140);
+  });
 });
